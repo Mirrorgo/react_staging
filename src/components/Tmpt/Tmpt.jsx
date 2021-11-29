597611d601be8df431e87655e20d85428308bfd6
mirrorgo <cloudmirrorgo@163.com>
2021-11-29 17:37:48 +0800

    2021年11月29日 all

---
+++ src/components/Tmpt/Tmpt.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Tmpt extends Component {
+  render() {
+    return (
+      <div>
+        <h2>真不错</h2>
+      </div>
+    );
+  }
+}
+
+export default Tmpt;
