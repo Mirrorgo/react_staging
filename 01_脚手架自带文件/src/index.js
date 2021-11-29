783af358de1e382367b88a7a7d2af79cf31307e9
mirrorgo <cloudmirrorgo@163.com>
2021-11-29 15:27:36 +0800

    2021年11月29日 1

---
+++ 01_脚手架自带文件/src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+// 👆这里的<React.StrictMode>能帮忙检查代码中不合理的地方(不是严格模式!!!)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
