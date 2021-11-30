6b77b1c5916d857f837621f1db7c601aa05149fa
mirrorgo <cloudmirrorgo@163.com>
2021-11-30 19:53:42 +0800

    2021年11月30日 first

--- 02_src_hello/index.js
+++ 02_src_hello/index.js
@@ -1,8 +1,8 @@
-//引入React核心库
+// 引入核心库
 import React from "react";
 // 引入ReactDOM
 import ReactDOM from "react-dom";
-// 引入App
+// 引入App组件  后缀为.js时可以省略
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
